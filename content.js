92de9e6180c21befee554a6c17476fa5f7824885
salah_yousef <salah.yousef@pharmatolia.com>
2020-02-04 15:24:50 +0300

    yes

--- content.js
+++ content.js
@@ -1,15 +1,9 @@
-console.log("Chrome extension host go?");
-
 chrome.runtime.onMessage.addListener(gotMessage);
 
 function gotMessage(message, sender, sendResponse) {
-
   const netflixPlayer = document.getElementsByClassName('VideoContainer')[0].firstChild.firstChild.firstChild;
   let currentTime = netflixPlayer.currentTime;
   chrome.storage.sync.set({ key: currentTime });
   localStorage.setItem('currentTime', currentTime * 1000);
-
-  console.log(message);
-  console.log(currentTime);
 }
 
