25ecfec07e0479d3b85f81a51773f75b2cde44ca
salah_yousef <salah.yousef@pharmatolia.com>
2020-02-04 15:02:33 +0300

    werer

--- content.js
+++ content.js
@@ -3,25 +3,12 @@ console.log("Chrome extension host go?");
 chrome.runtime.onMessage.addListener(gotMessage);
 
 function gotMessage(message, sender, sendResponse) {
-  /* console.log(window.netflix);
-  const videoPlayer = window.netflix.appContext.state.playerApp.getAPI().videoPlayer;
 
-  // Getting player id
-  const playerSessionId = videoPlayer.getAllPlayerSessionIds()[0];
-
-  const player = videoPlayer.getVideoPlayerBySessionId(playerSessionId);
-
-  let currentTime = player.getCurrentTime();
-  
- */
   const netflixPlayer = document.getElementsByClassName('VideoContainer')[0].firstChild.firstChild.firstChild;
   let currentTime = netflixPlayer.currentTime;
-  if (message == "host") {
-    chrome.storage.sync.set({ key: currentTime });
-    localStorage.setItem('currentTime', currentTime);
-  } else {
-    //player.seek(currentTime)
-  }
+  chrome.storage.sync.set({ key: currentTime });
+  localStorage.setItem('currentTime', currentTime * 1000);
+
   console.log(message);
   console.log(currentTime);
 }
